4d80e035d74a183430ba38528d7cae6d583951c1
Evan <ecomton3@gmail.com>
2013-04-09 09:27:45 -0400

    restyled, added audio icon

--- argon/js/argonFunctions.js
+++ argon/js/argonFunctions.js
@@ -2,96 +2,97 @@
  * @author evan
  */
 var objCoords = {
-	latitude : 33.772291,
-	longitude : -84.392633
+	latitude: 33.778464, longitude: -84.396887
+	//latitude: 33.770956, longitude: -84.391598
 }
 var cssObject;
 var mode = "Home";
 var index = 1, NUMINFO = 2;
-var myAppController = {
-	onArgonReady : function() {
-		// To test goto http://artnotart.org/jdbolter/argon2test/placmark.html
-		// create a div
+var myAppController =
+            {                   
+                onArgonReady : function()
+                {
+                    // To test goto http://artnotart.org/jdbolter/argon2test/placmark.html
+					// create a div
+						
+						  
+							   var divEl = document.createElement('div');
+		
+					// you have a div now, in devEl. content goes here  
+					// for the simple demo, just a green square with some text
+							   divEl.id = "cssContent";
+							   divEl.style.width = "100px";
+							   divEl.style.height = "100px";
+							   divEl.style.backgroundColor = "blue";
+							   divEl.style.position = 'absolute';
+							   divEl.style.fontSize = "16px";
+							   divEl.style.WebkitTransform = "scale3d(-1, 1, 1)";
+							   divEl.innerText = "AR + HTML5";
+		
+					//add an image
+						var oImg = document.createElement('img');
+							oImg.setAttribute('src','http://artnotart.org/jdbolter/decatur/decatur.0000.jpg');	
+							oImg.setAttribute('height','100');	
+							oImg.setAttribute('width','100');	
+							divEl.appendChild(oImg);
+							
+					// This will actually display the div on the screen
+					// be sure to comment out the cssObject to test this
+					// document.body.appendChild(divEl)		
+							
+					// create a CSS Object in the scene graph
+						  
+						  cssObject = new THREE.CSSObject(divEl); 
+		
+					// the width and height is used to align things.
+					 		cssObject.width = 10;
+					 		cssObject.height = 10;
+					 	  //  cssObject.position.x = 167;
+							//cssObject.position.y = 50.0;
+						  // 	cssObject.position.z = 157;
+						   
+				
+			//	console.log(curCoords);
+			
+			var location = {y:0,x:0,z:0};
+				var lla = ARGON.geolocation.getLLA();
+var lat = lla.latitude;
+var lon = lla.longitude;
 
-		var divEl = document.createElement('div');
+				location.y = 50;
+				
+				location.x = (objCoords.longitude - lon) * 1000000;
+				alert(location.x);
+				location.z = (objCoords.latitude - lat) * 1000000;
+						   cssObject.position.x = location.x;
+				cssObject.position.y = location.y;
+				cssObject.position.z = location.z;
+				alert(cssObject.position.x);
+					
+					
+					//attach directly to world. the x-y-z position should be relative to user
+							   
+					ARGON.World.add(cssObject);
+			   		
+					//  it may also be possible to attach the cssOjbect to a geoObject
+		 			//  geoObject : null,
+					//  near Chocolatte in Decatur
+	            	//  geoObject = ARGON.createGeoObject(33.791345,-84.305655, 0);
+		   			//  attach to world?
+                 	//  geoObject.add( cssObject );
+                 	//  ARGON.World.add( geoObject );			   
+							   
+					// Make is a tracked object if you are tracking off a marker. 
+					//		   trackedObject.add (cssObject);                    
+                }
 
-		// you have a div now, in devEl. content goes here
-		// for the simple demo, just a green square with some text
-		divEl.id = "cssContent";
-		divEl.style.width = "100px";
-		divEl.style.height = "100px";
-		divEl.style.backgroundColor = "blue";
-		divEl.style.position = 'absolute';
-		divEl.style.fontSize = "16px";
-		divEl.style.WebkitTransform = "scale3d(-1, 1, 1)";
-
-		divEl.innerText = "AR + HTML5";
-
-		//add an image
-		var oImg = document.createElement('img');
-		oImg.setAttribute('src', 'http://artnotart.org/jdbolter/decatur/decatur.0000.jpg');
-		oImg.setAttribute('height', '100');
-		oImg.setAttribute('width', '100');
-		divEl.appendChild(oImg);
-
-		// This will actually display the div on the screen
-		// be sure to comment out the cssObject to test this
-		// document.body.appendChild(divEl)
-
-		// create a CSS Object in the scene graph
-
-		cssObject = new THREE.CSSObject(divEl);
-
-		// the width and height is used to align things.
-		cssObject.width = 10;
-		cssObject.height = 10;
-		//  cssObject.position.x = 167;
-		//cssObject.position.y = 50.0;
-		// 	cssObject.position.z = 157;
-
-		//	console.log(curCoords);
-
-		var location = {
-			y : 0,
-			x : 0,
-			z : 0
-		};
-		var lla = ARGON.geolocation.getLLA();
-		var lat = lla.latitude;
-		var lon = lla.longitude;
-
-		location.y = 50;
-
-		location.x = (objCoords.longitude - lon) * 1000000;
-		//alert(location.x);
-		location.z = (objCoords.latitude - lat) * 1000000;
-		cssObject.position.x = location.x;
-		cssObject.position.y = location.y;
-		cssObject.position.z = location.z;
-		//alert(cssObject.position.x);
-
-		//attach directly to world. the x-y-z position should be relative to user
-
-		ARGON.World.add(cssObject);
-
-		//  it may also be possible to attach the cssOjbect to a geoObject
-		//  geoObject : null,
-		//  near Chocolatte in Decatur
-		//  geoObject = ARGON.createGeoObject(33.791345,-84.305655, 0);
-		//  attach to world?
-		//  geoObject.add( cssObject );
-		//  ARGON.World.add( geoObject );
-
-		// Make is a tracked object if you are tracking off a marker.
-		//		   trackedObject.add (cssObject);
-	}
-	// This would be for tracking, for example.
-	//   onDataSetLoaded : function(event)
-	//  {
-	//
-	//  }
-
-};
+				// This would be for tracking, for example.
+             	//   onDataSetLoaded : function(event)
+              	//  {
+  				//
+              	//  }
+	
+            };
 
 document.addEventListener("AR.DataSetLoadedEvent", myAppController.onDataSetLoaded);
 document.addEventListener("AR.DataSetActivatedEvent", myAppController.onDataSetActivated);
@@ -102,6 +103,7 @@ var context = new webkitAudioContext();
 var gainNode = context.createGainNode(),
  panner = context.createPanner(),
  objCoords = {latitude: 33.778464, longitude: -84.396887},
+ //	latitude: 33.772191,longitude: -84.393144},
  curCoords = {},
  playing = false,
  paused = false;
@@ -173,6 +175,8 @@ $(document).ready(function() {
   	//$('#mainNav').css('margin-top', $(window).height() - 120);
   	$('section').css('display','none');
   	$('#home').css('display','block');
+  	$('#mainNav .nav li').removeClass('active');
+  	$('#homeLink').addClass('active');
   });
   
   $('.listenBtn').click(function(e) {
@@ -182,6 +186,9 @@ $(document).ready(function() {
   //	$('.btn-group').css('margin-top', $(window).height()  - 150);
   	$('section').css('display','none');
   	$('#listen').css('display','block');
+  	$('#mainNav .nav li').removeClass('active');
+  	$('#homeLink').removeClass('active');
+  	$('#listenLink').addClass('active');
   });
   
   $('.learnBtn').click(function(e) {
@@ -190,6 +197,9 @@ $(document).ready(function() {
   //	$('#mainNav').css('margin-top', $(window).height() - 120);
   	$('section').css('display','none');
   	$('#learn').css('display','block');
+  	$('#mainNav .nav li').removeClass('active');
+  	$('#homeLink').removeClass('active');
+  	$('#learnLink').addClass('active');
   });
   
   $('.discoverBtn').click(function(e) {
@@ -198,6 +208,9 @@ $(document).ready(function() {
   	//$('#mainNav').css('margin-top', $(window).height() - 120);
   	$('section').css('display','none');
   	$('#discover').css('display','block');
+  	$('#mainNav .nav li').removeClass('active');
+  	$('#homeLink').removeClass('active');
+  	$('#discoverLink').addClass('active');
   });
   
 });
@@ -231,7 +244,7 @@ function refresh() {
 	setTimeout(refresh, 10000);
 }
 
-setTimeout(refresh, 10000);
+//setTimeout(refresh, 10000);
 
 
 function calcLocation() {
@@ -249,6 +262,13 @@ var lon = lla.longitude;
 	location.x = (objCoords.longitude - lon) * 1000;
 	location.z = (objCoords.latitude - lat) * 1000;
 	gainNode.gain.value = 1 - Math.abs(location.z / 10); 
+	if(gainNode.gain.value < 0.3) {
+		$('#audioIcon').css('background-image','url("res/audioLow.png")');
+	} else if(gainNode.gain.value >= 0.3 && gainNode.gain.value < 0.7) {
+		$('#audioIcon').css('background-image','url("res/audioMed.png")');
+	} else if(gainNode.gain.value > 0.7) {
+		$('#audioIcon').css('background-image','url("res/audioHigh.png")');
+	}
 	console.log(location);
 	
 	// straight ahead, 0.0 is striaght, negative left, positive right
